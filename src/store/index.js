60ca84b2a6ae445f3ceef202423706f351e9b049
Bilal <bks1733@gmail.com>
2023-04-21 18:18:33 +0530

    Using Redux Data in React Components.

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import React, { createStore } from 'react';
+import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'increment') {
