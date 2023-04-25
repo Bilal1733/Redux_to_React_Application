b5a379c3bb91343c780879d271110061b2163ce2
Bilal <bks1733@gmail.com>
2023-04-25 12:31:13 +0530

    Reading & Dispatching From A New Slice.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,40 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { authActions } from '../store';
+
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.logout());
+  };
+
+  const navbar = (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">My Products</a>
+        </li>
+        <li>
+          <a href="/">My Sales</a>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && navbar}
     </header>
   );
 };
